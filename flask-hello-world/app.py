ebac78e8f61f030bf7fd599662a9e4ec6e2b621a
Andrea Caroggio <andrea.caroggio@gmail.com>
2014-12-27 17:16:04 +0100

    flask-hello-world

---
+++ flask-hello-world/app.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+# ---- Flask Hello World ---- #
+
+# import the Flask class from the flask module
+from flask import Flask
+
+# create the application object - app è il nome dell'istanza flask appena creata
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route("/")
+@app.route("/hello")
+@app.route("/sea")
+
+# define the view using a function, which returns a string
+def hello_world():
+	return "Hello, World!"
+
+# start the development server using the run() method
+if __name__ == "__main__":
+	app.run()
